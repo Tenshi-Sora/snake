5ad80d65310f39f70e5bf0820a2fc1552b67b766
Tenshi-Sora <76926133+Tenshi-Sora@users.noreply.github.com>
2025-03-20 21:50:46 -0400

    Created the basic files and basic snake.py code

---
+++ snake.py
@@ -0,0 +1,9 @@
+from game_files import ui
+from game_files import game_settings
+from game_files import snake_logic
+from game_files import rat_logic
+from game_files import maze_logic
+
+def main():
+    pass
+
